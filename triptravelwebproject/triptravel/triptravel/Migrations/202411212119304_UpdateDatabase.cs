8566bf83c8c2ee2d5ca34b35a57bacf9efbcd051
Ahmet Eren Özcan <mersin.33ahmete@gmail.com>
2024-11-26 00:32:21 +0300

    asp.net mvc tatil seyahat
    asp.net mvc ile tatil seyahat bloğu web sitesi geliştirilmiştir

---
+++ triptravelwebproject/triptravel/triptravel/Migrations/202411212119304_UpdateDatabase.cs
@@ -0,0 +1,18 @@
+﻿namespace triptravel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDatabase : DbMigration
+    {
+        public override void Up()
+        {
+          //  AddColumn("dbo.Blogs", "VideoUrl", c => c.String());
+        }
+        
+        public override void Down()
+        {
+           // DropColumn("dbo.Blogs", "VideoUrl");
+        }
+    }
+}
